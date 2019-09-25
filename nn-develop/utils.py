7801ee728fc1d595afe3737730671783bc22edd7
jmguerra444 <guerrajorge93@gmail.com>
2019-09-25 17:30:23 +0200

    Add ConsoleFormat

--- nn-develop/utils.py
+++ nn-develop/utils.py
@@ -4,7 +4,34 @@ import os
 def loadSettings():
     
     thisComputer = os.environ['COMPUTERNAME']
-    with open("env-settings.json") as json_file:
+    with open("configuration.json") as json_file:
         data = json.load(json_file)
     
     return data[thisComputer]
+
+class CF:
+    """
+    CF (ConsoleFormat)
+    ------
+    Console ourput formater. Example:
+        >>> print(CF.w + "Warning: No active frommets remain. Continue?" + CF.e)
+        
+    `blue : OKBLUE`
+    `green : OKGREEN`
+    `h : HEADER`
+    `w : OKWARNING`
+    `f : FAIL`
+    `e : ENDC`
+    `b : BOLD`
+    `u : UNDERLINE`
+
+    Stolen from : https://stackoverflow.com/a/287944/7474885
+    """
+    blue = '\033[94m'     # OKBLUE
+    green = '\033[92m'    # OKGREEN
+    h = '\033[95m'        # HEADER
+    w = '\033[93m'        # OKWARNING
+    f = '\033[91m'        # FAIL
+    e = '\033[0m'         # ENDC
+    b = '\033[1m'         # BOLD
+    u = '\033[4m'         # UNDERLINE
