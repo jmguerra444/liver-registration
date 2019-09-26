40de42b84584fcb6cf60ea798d6b8ffa915bece5
jmguerra444 <guerrajorge93@gmail.com>
2019-09-26 18:09:28 +0200

    Update testing file

--- nn-develop/test_data_loader.py
+++ nn-develop/test_data_loader.py
@@ -5,10 +5,12 @@ s = loadSettings()
 
 dataLoader = DataLoaderJSON(jsonFile = "01-dataset.json",
                             pathPrefix = s["decathlon-dataset-path"],
-                            batch = 1, 
-                            validation = 0.1
+                            batchSize = 1, 
+                            validation = 21
                             )
 dataLoader.shuffleImages()
 dataLoader.splitTrainigData()
 
-print("Done!")
+for i in range(20):
+    reqI = dataLoader.getBatch("Tr", True)
+    print(reqI)
