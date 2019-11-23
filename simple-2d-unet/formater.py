9190ea3fd7183d1155a26295a4a4638daa226cd4
jmguerra444 <guerrajorge93@gmail.com>
2019-11-23 12:46:36 +0100

    Add console formater class

---
+++ simple-2d-unet/formater.py
@@ -0,0 +1,56 @@
+class FontStyle:
+    """
+    FontStyle
+    ------
+    Console ourput formater. Example:
+        >>> print(FontStyle.w + "Warning: No active frommets remain. Continue?" + FontStyle.e)
+    Or as static member funcions
+        >>> FontStyle.printbl("this is a blue text")
+        
+    `blue : OKBLUE`
+    `green : OKGREEN`
+    `h : HEADER`
+    `w : OKWARNING`
+    `f : FAIL`
+    `e : ENDC`
+    `b : BOLD`
+    `u : UNDERLINE`
+
+    Stolen from : https://stackoverflow.com/a/287944/7474885
+    """
+    blue = '\033[94m'     # OKBLUE
+    green = '\033[92m'    # OKGREEN
+    h = '\033[95m'        # HEADER
+    w = '\033[93m'        # OKWARNING
+    f = '\033[91m'        # FAIL
+    b = '\033[1m'         # BOLD
+    u = '\033[4m'         # UNDERLINE
+    e = '\033[0m'         # ENDC
+
+    @staticmethod
+    def printbl(myText):
+        print('\033[94m', myText, '\033[0m')
+    
+    @staticmethod
+    def printgr(myText):
+        print('\033[92m', myText, '\033[0m')
+    
+    @staticmethod
+    def printh(myText):
+        print('\033[95m', myText, '\033[0m')
+    
+    @staticmethod
+    def printw(myText):
+        print('\033[93m', myText, '\033[0m')
+    
+    @staticmethod
+    def printf(myText):
+        print('\033[91m', myText, '\033[0m')
+    
+    @staticmethod
+    def printb(myText):
+        print('\033[1m', myText, '\033[0m')
+    
+    @staticmethod
+    def printu(myText):
+        print('\033[4m', myText, '\033[0m')
