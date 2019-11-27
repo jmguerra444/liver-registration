e3c0b0f260e693133cbaa611f867fcf8d0331e0b
jmguerra444 <guerrajorge93@gmail.com>
2019-11-28 00:37:39 +0100

    DatasetHandler with preprocessing options

--- simple-2d-unet/data_loader.py
+++ simple-2d-unet/data_loader.py
@@ -14,38 +14,37 @@ from imageio import imread
 import torch
 from torch.utils.data.dataset import Dataset
 from torchvision import transforms
+import torchvision.transforms.functional as tf
 
 from utils import *
+from helper import LoaderOptions
 from console import Console as con
 
 # %%
-class DataLoaderManager(Dataset):
+class DatasetHandler(Dataset):
     """
-    ### DataLoaderManager
+    ### DatasetHandler (from .PNG)
     ----
     - Layer between raw image paths and torch.dataloader, this class should also handle
     required preprocessing operations.
     - For PNG image loading class
+    - For training only
     \n
     #### Params
     `imagesPath` : List of paths that reference the images \n
     `labelsPath` : List of paths that refrence the labels
     """
     
-    def __init__(self, imagesPath, labelsPath):
+    def __init__(self, imagesPath, labelsPath, options = LoaderOptions()):
         """
-        Constructor for simple Image loader
+        Constructor for simple Image loader,
         """
+        
         self.trainImages = imagesPath                       # Full train image paths
         self.trainLabels = labelsPath                       # Full train label paths
-        
-        self.imageTransforms = transforms.Compose(
-            [transforms.ToTensor()]
-            )
-        # Queue more image transformations here, crop, center etc. etc. 
+        self.options = options                              # Preprocessing operations
 
     def __getitem__(self, index):
-        
         """
         This function needs to be overriten from Dataset(torch) class
         """
@@ -53,24 +52,52 @@ class DataLoaderManager(Dataset):
         image = imread(self.trainImages[index])
         label = imread(self.trainLabels[index])
         
-        imageTensor = self.imageTransforms(image)
-        labelTensor = self.imageTransforms(label)
+        imageTensor, labelTensor = self.transformations(image, label)
         
         return imageTensor, labelTensor
-        
+
     def __len__(self):
         return len(self.trainImages)
 
+    def transformations(self, image, label):
+        """
+        Chain of required imaged transformations inc. `.toTensor()`, does all operations according
+        to LoaderOptions
+        """
+
+        image = tf.to_pil_image(image)
+        label = tf.to_pil_image(label)
+        
+        # Do all queued operations ...
+        
+        # Resize
+        if (self.options.imageSize != None):
+            size = self.options.imageSize
+            image = tf.resize(image, size = (size, size), interpolation = 2)
+            label = tf.resize(label, size = (size, size), interpolation = 0)
+        
+        image = tf.to_tensor(image)
+        label = tf.to_tensor(label)
+        
+        return image, label
+
 # %%
-settings = loadSettings("configuration.json")
+def test_1():
+    settings = loadSettings()
 
-# TODO : Check images loding as a list of lists
-imagesPath = loadFromCSV(settings["decathlon-output-png"] + "/images-list.csv")[0]
-labelsPath = loadFromCSV(settings["decathlon-output-png"] + "/labels-list.csv")[0]
+    # TODO : Check images loding as a list of lists
+    imagesPath = loadFromCSV(settings["decathlon-output-png"] + "/images-list.csv")[0]
+    labelsPath = loadFromCSV(settings["decathlon-output-png"] + "/labels-list.csv")[0]
 
-dlm = DataLoaderManager(imagesPath = imagesPath,
-                        labelsPath = labelsPath)
+    dh = DatasetHandler(imagesPath = imagesPath,
+                        labelsPath = labelsPath,
+                        options = LoaderOptions(imageSize = 64))
+    image, label = dh.__getitem__(10)         # Has the image as tensor
+    plt.subplot(1, 2, 1)
+    plt.imshow(image.numpy().squeeze())
+    plt.subplot(1, 2, 2)
+    plt.imshow(label.numpy().squeeze())
+    
+    return image, label
 
-image = dlm.__getitem__(10)
-label = dlm.__getitem__(10)
-# %%
+image, label = test_1()
