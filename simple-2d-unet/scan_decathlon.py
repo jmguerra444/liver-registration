87af533c5041b875bb3bd89e2429e98a5beb017b
Jorge Mario Guerra González <jorge.guerra@tum.de>
2019-12-09 15:43:06 +0100

    Split volume paths during scan

--- simple-2d-unet/scan_decathlon.py
+++ simple-2d-unet/scan_decathlon.py
@@ -64,7 +64,6 @@ def scanTrainDataset(pathPrefix,
     for sample in paths[0: samples]:
         volume = np.array(nibabel.load(sample["image"]).get_fdata(), dtype = np.float32)
         labels = np.array(nibabel.load(sample["label"]).get_fdata(), dtype = np.float32)
-                                                                     
         if (volume.shape[2] == labels.shape[2]):
             for sliceIndex in range(volume.shape[2]):
                 image = volume[:, :, sliceIndex]
@@ -115,35 +114,54 @@ def saveAsPNG(outputDirectory,
     labels = np.zeros((1,1))
     imageFilenames = []
     labelFilenames = []
+    studyId = 0
+
+    thisVolumePaths = []
+    thisLabelsPaths = []
+
+    os.makedirs("{}/image".format(outputDirectory), exist_ok = True)
+    os.makedirs("{}/label".format(outputDirectory), exist_ok = True)
     
     for index in range(len(imagePaths)):
         imagePath = imagePaths[index]
         s = int(indexes[index])                     # The slice index
         
-        if (imagePath != path):
+        if (imagePath != path): # There is a new volume
             path = imagePath
+            
+            if (index != 0):
+                imageFilenames.append(thisVolumePaths)
+                labelFilenames.append(thisLabelsPaths)
+                studyId += 1
+                
+            thisVolumePaths = []
+            thisLabelsPaths = []
+            
             con.printbl("Doing {}".format(path))
+            os.makedirs("{}/label/{:02d}".format(outputDirectory, studyId), exist_ok = True)
+            os.makedirs("{}/image/{:02d}".format(outputDirectory, studyId), exist_ok = True)
             
             volume = np.array(nibabel.load(path).get_fdata(), dtype = np.float32)
             labels = np.array(nibabel.load(labelPaths[index]).get_fdata(), dtype = np.float32)
             volume = normalizeSample(volume)        # In range [0 1]
-            
-            
+
+        imageFilename = "{}/image/{:02d}/image{:06d}.png".format(outputDirectory, studyId, index)
+        labelFilename = "{}/label/{:02d}/label{:06d}.png".format(outputDirectory, studyId, index)
+        thisVolumePaths.append(imageFilename)
+        thisLabelsPaths.append(labelFilename)
+        
         image = np.uint8((volume[:, :, s]) * 255)   # Range [0 255]
         label = np.uint8((labels[:, :, s]) * 50)    # Increase constrst on labels (visually), TODO : Remove
-        
-        imageFilename = "{}/image/image{:06d}.png".format(outputDirectory, index)
-        labelFilename = "{}/label/label{:06d}.png".format(outputDirectory, index)
         
         imwrite(imageFilename, image)
         imwrite(labelFilename, label)
         
-        imageFilenames.append(imageFilename)
-        labelFilenames.append(labelFilename)
         #####################
         # MAYBE :  Do some image operations
         #####################
-        
+    
+    imageFilenames.append(thisVolumePaths)
+    labelFilenames.append(thisLabelsPaths)
     listImagesFilename = "{}/images-list.csv".format(outputDirectory)
     listLabelsFilename = "{}/labels-list.csv".format(outputDirectory)
     
@@ -160,7 +178,7 @@ def saveAsPNG(outputDirectory,
 #%% Load folder settings
 settings = loadSettings("configuration.json")
 
-#%%
+# %%
 # Scan nifti filles and save in csv file all paths and indexes of image and label
 scanTrainDataset(pathPrefix = settings["decathlon-dataset-path"],
                 datasetSchema = "01-dataset.json",
@@ -183,10 +201,13 @@ saveAsPNG(settings["decathlon-output-png"],
 con.printgr("Done!")
 
 # %%
-
+a = loadFromCSV('C:\Master thesis\master\data\medical-decathlon-png\images-list.csv')[0]
+print("done")
 # USE THIS
 
 # c:\Master thesis\master\simple-2d-unet\scan_decathlon.py:32: DeprecationWarning: `imsave` is deprecated!
 # `imsave` is deprecated in SciPy 1.0.0, and will be removed in 1.2.0.
 # Use ``imageio.imwrite`` instead.
 #   if (mode == "train"):
+
+# %%
