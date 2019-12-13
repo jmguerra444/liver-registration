fd7b6e76d0d89abfd442813ec06d38219a800f6a
Jorge Mario Guerra González <jorge.guerra@tum.de>
2019-12-13 20:41:52 +0100

    Argmax to dim = 0

--- simple-2d-unet/run.py
+++ simple-2d-unet/run.py
@@ -155,7 +155,7 @@ def test(model : UNet,
         
         image = image.cpu().detach().numpy()[0, 0, :, :]
         label = label.cpu().detach().numpy()[0, 0, :, :]
-        labelMap = np.argmax(prediction.cpu().detach().numpy()[0, :, :, :], 2)
+        labelMap = np.argmax(prediction.cpu().detach().numpy()[0, :, :, :], 0)
         
         collection.append(image)
         collection.append(label)
