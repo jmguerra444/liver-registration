1774c37bb1b6bfa252b37f86ffe913a268508fb6
Jorge Mario Guerra González <jorge.guerra@tum.de>
2019-12-16 17:37:06 +0100

    Fix whitespace

--- simple-2d-unet/unet.py
+++ simple-2d-unet/unet.py
@@ -31,22 +31,22 @@ class UNet(nn.Module):
         self.encoder3 = UNet._block(features * 2, features * 4, name = "encoder3")
         self.pool3 = nn.MaxPool2d(kernel_size = 2, stride = 2)
         
-        self.encoder4= UNet._block(features * 4, features * 8, name = "encoder4")
+        self.encoder4 = UNet._block(features * 4, features * 8, name = "encoder4")
         self.pool4 = nn.MaxPool2d(kernel_size = 2, stride = 2)
         
         self.bottleneck = UNet._block(features * 8, features * 16, name ="bottleneck")
         
         self.upconv4 = nn.ConvTranspose2d(features * 16, features * 8, kernel_size = 2, stride = 2)
-        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="decoder4")
+        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name = "decoder4")
         
         self.upconv3 = nn.ConvTranspose2d(features * 8, features * 4, kernel_size = 2, stride = 2)
-        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="decoder3")
+        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name = "decoder3")
         
         self.upconv2 = nn.ConvTranspose2d(features * 4, features * 2, kernel_size = 2, stride = 2)
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="decoder2")
+        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name = "decoder2")
         
         self.upconv1 = nn.ConvTranspose2d(features * 2, features, kernel_size = 2, stride = 2)
-        self.decoder1 = UNet._block(features * 2, features, name="decoder1")
+        self.decoder1 = UNet._block(features * 2, features, name = "decoder1")
 
         self.conv = nn.Conv2d(in_channels = features, out_channels = out_channels, kernel_size = 1)
     
@@ -60,16 +60,16 @@ class UNet(nn.Module):
         bottleneck = self.bottleneck(self.pool4(enc4))
 
         dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = torch.cat((dec4, enc4), dim = 1)
         dec4 = self.decoder4(dec4)
         dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = torch.cat((dec3, enc3), dim = 1)
         dec3 = self.decoder3(dec3)
         dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = torch.cat((dec2, enc2), dim = 1)
         dec2 = self.decoder2(dec2)
         dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = torch.cat((dec1, enc1), dim = 1)
         dec1 = self.decoder1(dec1)
         return torch.sigmoid(self.conv(dec1))
     
