Fix scaling issue

simple-2d-unet/data_loader.py
@@ -76,11 +76,13 @@ class DatasetHandler(Dataset):
         to LoaderOptions
         """
         # FIXME : Contrasts very bad
-        # label = image_as_uint(label / 50)                             # Compensate scaling factor
+        # label = label / 50                             # Compensate scaling factor
         
         image = tf.to_pil_image(image)
         label = tf.to_pil_image(label)
 
+        label = tf.adjust_brightness(label, 1 / 50)
+        
         # Resize
         if (self.options.imageSize != None):
             size = self.options.imageSize
@@ -88,7 +90,8 @@ class DatasetHandler(Dataset):
             label = tf.resize(label, size = (size, size), interpolation = 0)
         
         image = tf.to_tensor(image)
-        label = tf.to_tensor(label)
+        # label = tf.to_tensor(label)
+        label = torch.from_numpy(np.expand_dims(np.array(label), 0))
         
         return image, label
 