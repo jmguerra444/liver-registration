2a3bd23dc665203568c2c0c4fddcad8a6f314293
Jorge Mario Guerra González <jorge.guerra@tum.de>
2019-12-18 00:22:18 +0100

    Create keydetector.py

---
+++ tester/keydetector.py
@@ -0,0 +1,21 @@
+from pynput import keyboard
+import time
+from PyQt5.QtCore import QObject, pyqtSignal
+
+class KeyMonitor(QObject):
+    letterPressed = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.listener = keyboard.Listener(on_release = self.on_release)
+
+    def on_release(self,key):
+        if type(key) == keyboard._win32.KeyCode:
+            print(key.char)
+            self.letterPressed.emit(key.char.lower())
+
+    def stop_monitoring(self):
+        self.listener.stop()
+
+    def start_monitoring(self):
+        self.listener.start()
