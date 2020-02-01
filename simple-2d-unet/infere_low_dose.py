098cde58c4d7f512188e87641c7655dbf5bfae1b
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-02-02 00:50:09 +0100

    Add graphs into inference method

--- simple-2d-unet/infere_low_dose.py
+++ simple-2d-unet/infere_low_dose.py
@@ -1,3 +1,5 @@
+# %%
+print("starting")
 # %% Inference method
 import os
 import sys
@@ -43,7 +45,7 @@ args = Arguments(settings["Arguments"])
 
 
 modelPath = "C:/Master thesis/master/exported-results/12181327/12181327-004.pt"
-volumePath =  "C:/Users/Jorgue Guerra/Desktop/test_low_dose"
+volumePath =  "G:/selected data splited/MR/006"
 
 volume = dcmread(volumePath)
 
@@ -54,10 +56,13 @@ unet.cuda()
 unet.eval()
 
 collection = []
+
+savePrediction = True
+saveMask = False
 with tqdm(total = volume.shape[2]) as t:
     
     # for slice_ in range(50, voqqlume.shape[2] - 90):
-    for slice_ in range(20, 70, 10):
+    for slice_ in range(0, 90):
         
         t.set_description("Slice : {}".format(slice_))
         image_ = volume[:, :, slice_]
@@ -65,9 +70,9 @@ with tqdm(total = volume.shape[2]) as t:
         image = np.float32(image_)
         image = tf.to_pil_image(image)
         image = image.transpose(Image.FLIP_TOP_BOTTOM)
-        image = tf.resize(image, size = (256, 256), interpolation = 2)
         image_ = image # Save image "status" to display
         
+        image = tf.resize(image, size = (256, 256), interpolation = 2)
         image = tf.to_tensor(image)
         image = image.unsqueeze(0)
         image = image.to(args.device)
@@ -75,13 +80,29 @@ with tqdm(total = volume.shape[2]) as t:
         
         pred = unet(image)
         pred = np.argmax(pred.cpu().detach().numpy()[0, :, :, :], 0)
-        pred = resize(pred, (512, 512))
+        pred = resize(pred, (512, 512), preserve_range = True, order = 0)
         
-        plt.subplot(1, 2, 1)
-        plt.imshow(image_)
-        plt.subplot(1, 2, 2)
-        plt.imshow(pred)
-        plt.show()
+        if savePrediction:
+            plt.subplot(1, 2, 1)
+            plt.imshow(image_)
+            plt.axis('off')
+            plt.subplot(1, 2, 2)
+            plt.imshow(pred)
+            plt.axis('off')
+            plt.tight_layout()
+            plt.savefig("{}/out/{:03d}.png".format(volumePath, slice_))
+        
+        if saveMask:
+            image_ = np.array(image_)
+            mask = image_ * pred
+            plt.imshow(mask)
+            # plt.show()
+            
+            plt.figure()
+            plt.imshow(image_)
+            # plt.show()
+            print("bp")
+        # plt.show()
         
         # prediction[:, :, slice_] = pred
         
@@ -93,8 +114,7 @@ with tqdm(total = volume.shape[2]) as t:
         t.update()
 
 print("Donie!")
-# s = np.asarray(collection)
-# s = np.transpose(s, (2, 1, 0))
-# viewer(s)
+
+
 
 # %%
