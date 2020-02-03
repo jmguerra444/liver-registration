8d4285bc7fdb2328a4dcffb4b5e3c0c1f2a8805f
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-02-03 14:27:08 +0100

    Fix enviroment configuration issues

---
+++ install_packeges.bat
@@ -0,0 +1,11 @@
+conda install -y pandas
+conda install -y -c anaconda numpy
+conda install -y -c conda-forge tensorflow
+conda install -y -c conda-forge nibabel
+conda install -y -c conda-forge tqdm
+conda install -y -c conda-forge typed-ast
+conda install -y -c conda-forge imageio
+conda install -y -c conda-forge ipython
+conda install -y -c conda-forge matplotlib
+conda install -y -c conda-forge scikit-image
+conda install -y -c conda-forge pillow
