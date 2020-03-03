ee0d21b0f57ad4f678305781fc8db4c04efb0ee9
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-03-03 15:18:50 +0100

    Add model with parent
    (working ok)

--- imfusion-inference/trace-model/net2script3d_2.py
+++ imfusion-inference/trace-model/net2script3d_2.py
@@ -6,25 +6,22 @@ from unet import UNet
 from helper import loadSettings, Arguments, loadState
 
 
-# scripted_gate = torch.jit.script(UNet())
-# my_cell = MyCell(scripted_gate)
-# traced_cell = torch.jit.script(my_cell)
+import torch
+import torch.optim as optim
+import logging
 
-class Unet3D(torch.nn.Module):
+from unet import UNet
+from helper import loadSettings, Arguments, loadState
 
+
+class Parent(torch.nn.Module):
     def __init__(self, unet):
-        super(Unet3D, self).__init__()
-        p = torch.zeros(1, 1, 256, 256)
-        self.traced_net = torch.jit.trace(unet, p)
+        super(Parent, self).__init__()
+        self.unet = unet
 
-    def forward(self, x):
-        out = torch.zeros(1, 2, 20, 256, 256)
-        for i in range(x.size(2)):
-            im = x[:, :, i, :, :]
-            pred = unet(im)
-            out[:, :, i, :, :] = pred
-            print(i)
-        return out
+    def forward(self, ph):
+        print("Doing parent")
+        return self.unet(ph)
 
 settings_path = "C://Master thesis//master//2d-unet-liver-ct//configuration.json"
 model_path = "C://Master thesis//master//exported-results//02222159//02222159-014.pt"
@@ -35,19 +32,8 @@ args = Arguments(settings["Arguments"])
 unet = UNet(**settings["2d-unet-params"])
 optimizer = optim.Adam(unet.parameters())
 unet, optimizer, epoch, trainLoss, validLoss = loadState(model_path, unet, optimizer, args)
-# unet.cuda()
-# unet.eval()
 
-# imag = torch.zeros(1, 1, 256, 256)
-# traced_net = torch.jit.trace(unet, imag)
-# traced_net.save("lowdose-liver-fake.pt")
-# print(traced_net.code)
-# print(traced_net.graph)
-
-volu = torch.zeros(1, 1, 20, 256, 256)
-# volu = volu.to('cuda:0')
-unet3 = Unet3D(unet)
-
-
-traced_unet3 = torch.jit.trace(unet3, volu)
-traced_unet3.save("3d-TEST-50.pt")
+ph = torch.zeros(1, 1, 256, 256)
+parent = Parent(unet)
+traced_net = torch.jit.trace(parent, ph)
+traced_net.save("..//model-parent.pt")
