aaf5fb683dd84fe2b8c9662b30c3e5b40d52b1ff
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-05-14 16:59:33 -0500

    Add mri volumetry script

---
+++ kri-evaluation/workspaces/volumetry_mri.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+import datetime
+import json
+
+from utils import getData, log
+
+data = getData()
+workspace = "C://Master thesis//master//kri-evaluation//workspaces//base-mri.iws"
+filename = "C://Master thesis//master//kri-evaluation//plugin-volumetry-mri.txt"
+
+log("STARTING PROCESS {}".format(str(datetime.datetime.now())), filename)
+
+mri_data = data["diagnostic-mr"]
+mesh_data = data["diagnostic-mr-mesh"]
+
+for index, i in enumerate(mri_data):
+    print("Now doing {}".format(i))
+    log(i, filename)
+    p = subprocess.Popen('ImFusionSuite "{}" Data="{}" Mesh="{}"'.format(workspace, i, mesh_data[index]))
+    p.wait()
