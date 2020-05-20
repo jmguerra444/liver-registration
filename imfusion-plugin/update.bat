35b0e49ee89ecda2740b10bb0b1ef4b3884a5926
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-05-19 21:46:51 -0500

    Add script to compiple and update plugins

---
+++ imfusion-plugin/update.bat
@@ -0,0 +1,19 @@
+cd "C:\Master thesis\master\imfusion-plugin"
+taskkill /IM ImFusionSuite.exe /T /F
+
+cd registration-plugin
+cmake --build build --config Release
+cd ..
+
+cd mri-plugin
+cmake --build build --config Release
+cd ..
+
+cd lowdose-plugin
+cmake --build build --config Release
+cd ..
+
+xcopy /y "C:\Master thesis\master\imfusion-plugin\registration-plugin\build\bin\Release\RegistrationPlugin.dll" "C:\Program Files\ImFusion\ImFusion Suite\Suite\plugins\RegistrationPlugin.dll"
+xcopy /y "C:\Master thesis\master\imfusion-plugin\mri-plugin\build\bin\Release\MRISegmentationPlugin.dll" "C:\Program Files\ImFusion\ImFusion Suite\Suite\plugins"
+xcopy /y "C:\Master thesis\master\imfusion-plugin\lowdose-plugin\build\bin\Release\LowDoseSegmentationPlugin.dll" "C:\Program Files\ImFusion\ImFusion Suite\Suite\plugins"
+start ImFusionSuite.exe
