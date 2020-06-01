eba9860868dbc2d4686c9374db0e29480d7a74af
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-05-31 23:27:07 -0500

    Add CE-CT evaluation

--- kri-evaluation/automatic-evaluation/volumetry_ct.py
+++ kri-evaluation/automatic-evaluation/volumetry_ct.py
@@ -12,9 +12,11 @@ filename = "C://Master thesis//master//kri-evaluation//plugin-volumetry-ct.txt"
 log("STARTING PROCESS {}".format(str(datetime.datetime.now())), filename)
 
 ct_data = data["diagnostic-ct"]
+ct_mesh = data["diagnostic-mesh-ct"]
 
 for index, i in enumerate(ct_data):
     print("Now doing {}".format(i))
     log(i, filename)
-    p = subprocess.Popen('ImFusionSuite "{}" Data="{}"'.format(workspace, i))
-    p.wait()
+    p = subprocess.Popen('ImFusionSuite "{}" Data="{}" Mesh="{}"'.format(workspace, i, ct_mesh[index]))
+    time.sleep(60)
+    p.terminate()
