b4c5144b9af86f1f25992078cab0838d1577b285
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-06-13 16:22:36 -0500

    Update script

--- registration-params/helper.py
+++ registration-params/helper.py
@@ -6,4 +6,5 @@ def setup(study):
     os.makedirs(study.get("screenshotFolder"), exist_ok = True)
     
     # Add description
+    log(study.get("workspaceFile"), study.get("descriptionFile"))
     log(study.get("description"), study.get("descriptionFile"))
--- registration-params/run.py
+++ registration-params/run.py
@@ -14,7 +14,7 @@ description = input("What's the description os this experiment?")
 study_id = now()
 study = {
     "id" : study_id,
-    "workspaceFile" : "ws.iws",
+    "workspaceFile" : "workspaces\\rigid_ncc.iws",
     "description": description,
     "descriptionFile" : absolutePath("studies\\{}\\description".format(study_id)),
     "screenshotFolder" : absolutePath("studies\\{}\\screenshots\\".format(study_id))
@@ -22,15 +22,16 @@ study = {
 setup(study)
 
 # %% Session
-log("STARTING PROCESS {}".format(now()), logger)
+log("\n",study.get("descriptionFile"))
+log("STARTING PROCESS {}".format(now()), study.get("descriptionFile"))
 for p in landmarks :
     
     # Stop condition
-    if (int(p) < 1000):
+    if (int(p) < 0):
         continue
-    
+
     if landmarks[p].get("annotated"):
-        log("Doing " + p, logger)
+        log("Doing " + p, study.get("descriptionFile"))
         mriPath = landmarks[p].get("mri-data") 
         ctPath = landmarks[p].get("ct-data")
         points1 = landmarks[p].get("points1")
@@ -43,8 +44,9 @@ for p in landmarks :
         
         process = subprocess.Popen(c)
         screenshotPath = "{}\\{}.png".format(study["screenshotFolder"], p)
-        screenshot(screenshotPath, 5)
-        process.wait()
+        screenshot(screenshotPath, 50) # Not asynchronous
+        process.terminate()
+        # process.wait()
 
 
 # %%
