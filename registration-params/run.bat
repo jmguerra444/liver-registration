f812814bda14d1f881d3806af6bbc51c46740067
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-06-15 20:13:34 -0500

    Create run.bat

---
+++ registration-params/run.bat
@@ -0,0 +1,12 @@
+python run.py --workspace="rigid_ssd.iws" ^
+              --description="Rigid registration with SSD similarity"
+
+python run.py --workspace="affine_mi.iws" ^
+              --description="Affine registration with MI similarity"
+
+python run.py --workspace="affine_ncc.iws" ^
+              --description="Affine registration with NCC similarity"
+
+python run.py --workspace="affine_ssd.iws" ^
+              --description="Affine registration with SSD similarity"
+
