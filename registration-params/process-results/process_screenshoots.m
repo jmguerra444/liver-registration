29b8c66aa3f3e91d1fdc72311146e8acc67a9b18
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-06-17 19:55:17 -0500

    Update process_screenshoots.m

--- registration-params/process-results/process_screenshoots.m
+++ registration-params/process-results/process_screenshoots.m
@@ -1,28 +1,54 @@
+folder_ids = {
+    'n_affine_mi_0616014924'
+    'n_affine_ncc_0616021035'
+    'n_affine_ssd_0616023146'
+    'n_rigid_mi_0616004549'
+    'n_rigid_ncc_0616010701'
+    'n_rigid_ssd_0616012811'
+    't_affine_mi_0617053946'
+    't_affine_ncc_0617072244'
+    't_affine_ssd_0617090541'
+    't_rigid_mi_0617003056'
+    't_rigid_ncc_0617021353'
+    't_rigid_ssd_0617035649'
+    };
 
-folder_id = '0613162951';
-study_folder = strcat('C:\Master thesis\master\registration-params\studies\', folder_id,'\screenshots\');
-
-list = dir(strcat(study_folder, '\*.png'));
-
-result = [];
-
-for i = 1 : length(list)
-    image_filename = list(i).name;
-    path = absolute_path(image_filename, study_folder);
+results = [];
+for i = 1 : length(folder_ids)
+    f = folder_ids{i};
+    n.name = f(1 : end - 11);
     
-    image = rgb2gray(imread(path));
-    image = image(537:553, 1688:1770);
-    image = imresize(image, 20);
-    text = ocr(image);
-    text = text.Text;
-    text = parse_text(text);
+    r = process_folder(f);
+    n.result = r;
     
-    d.name = strrep(image_filename, '.png', '');
-    d.distance = str2double(text);
-    
-    result = [result, d];
+    results = [results, n];
 end
 
+function result = process_folder(folder_id)
+    disp(folder_id)
+    study_folder = strcat('C:\Master thesis\master\registration-params\studies\completed\', folder_id,'\screenshots\');
+
+    list = dir(strcat(study_folder, '\*.png'));
+
+    result = [];
+
+    for i = 1 : length(list)
+        image_filename = list(i).name;
+        path = absolute_path(image_filename, study_folder);
+
+        image = rgb2gray(imread(path));
+        image = image(537:553, 1688:1770);
+        image = imresize(image, 20);
+        text = ocr(image);
+        text = text.Text;
+        text = parse_text(text);
+
+        d.name = strrep(image_filename, '.png', '');
+        d.(folder_id) = str2double(text);
+
+        result = [result, d];
+    end
+end
 
 function path = absolute_path(relative_path, parent)
     path = strcat(parent, relative_path);
