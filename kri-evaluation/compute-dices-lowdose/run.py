b1490e9053587da816b02168c63eeadc264c1fb2
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-07-02 16:16:54 -0500

    Add lowdose dice score computations

---
+++ kri-evaluation/compute-dices-lowdose/run.py
@@ -0,0 +1,35 @@
+import subprocess
+import time
+import pyscreenshot
+
+def screenshot(filename = "screenshot.png", delay_s = 0):
+    time.sleep(delay_s)
+    i  = pyscreenshot.grab()
+    i.save(filename)
+
+patients = [
+            "006", 
+            "007", 
+            "013", 
+            "017", 
+            "020", 
+            "021",
+            "025",
+            "030",
+            "036",
+            "037",
+            "040",
+            "065",
+            ]
+
+for p in patients:
+    lowdose = "C:/Users/Jorgue Guerra/OneDrive - SurgicEye GmbH/thesis/data/Selected MR data/{}/LOWDOSE-CT".format(p)
+    gt = "C:/Users/Jorgue Guerra/OneDrive - SurgicEye GmbH/thesis/data/Selected MR data/{}/ws_ct/Liver.stl".format(p)
+    print(lowdose)
+
+    c = 'ImFusionSuite workspace.iws ct="{}" gt="{}"'.format(lowdose, gt)
+
+    screenshotPath = "screenshots/{}.png".format(p)
+    process = subprocess.Popen(c)
+    screenshot(screenshotPath, 40)
+    process.terminate()
