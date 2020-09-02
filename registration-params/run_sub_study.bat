c414f81f9143917c4b67725b7093990f833ca813
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-09-02 15:32:22 +0200

    Update run_sub_study.bat

--- registration-params/run_sub_study.bat
+++ registration-params/run_sub_study.bat
@@ -1,5 +1,3 @@
-ECHO CONTROL POINTS
-
 REM python run.py --workspace="su_points_333.iws" ^
 REM               --description="Substudy for number of point controls 333" ^
 REM               --timer=180
@@ -12,7 +10,6 @@ REM python run.py --workspace="su_points_555.iws" ^
 REM               --description="Substudy for number of point controls 555" ^
 REM               --timer=600
 
-
 REM python run.py --workspace="su_smooth_0.iws" ^
 REM               --description="Substudy for number of point controls 444" ^
 REM               --timer=500
@@ -21,11 +18,11 @@ REM python run.py --workspace="su_smooth_01.iws" ^
 REM               --description="Substudy for number of point controls 444" ^
 REM               --timer=500
 
-REM ECHO STEPS
 REM python run.py --workspace="su_smooth_0001.iws" ^
 REM               --description="Substudy for number of point controls 444" ^
 REM               --timer=500
 
+REM ECHO STEPS
 REM python run.py --workspace="so_step_1.iws" ^
 REM               --description="Substudy for number of point controls 444" ^
 REM               --timer=500
@@ -35,5 +32,9 @@ REM               --description="Substudy for number of point controls 444" ^
 REM               --timer=360
 
 REM python run.py --workspace="so_step_10.iws" ^
+REM               --description="Substudy for number of point controls 444" ^
+REM               --timer=500
+
+REM python run.py --workspace="so_step_20.iws" ^
 REM               --description="Substudy for number of point controls 444" ^
 REM               --timer=500
