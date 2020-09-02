7855bd6d9a8dd0e511fbdd930f950908cb8b4e08
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-09-02 14:08:16 +0200

    Add fine landmarks

---
+++ registration-params/run_all_fine.bat
@@ -0,0 +1,11 @@
+python run.py --workspace="td_ffd_ncc.iws" ^
+              --description="Free form deformation with ssd similarity" ^
+              --timer=180
+
+python run.py --workspace="td_ffd_ssd.iws" ^
+              --description="Free form deformation with ssd similarity" ^
+              --timer=180
+
+python run.py --workspace="td_ffd_mi.iws" ^
+              --description="Free form deformation with ssd similarity" ^
+              --timer=180
