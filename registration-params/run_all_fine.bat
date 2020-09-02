05208f445b81f4d73933c2348b25dbea36eaa75f
Jorge Mario Guerra González <jorge.guerra@tum.de>
2020-09-02 15:32:17 +0200

    Update substudy WS

--- registration-params/run_all_fine.bat
+++ registration-params/run_all_fine.bat
@@ -1,11 +1,42 @@
-python run.py --workspace="td_ffd_ncc.iws" ^
-              --description="Free form deformation with ssd similarity" ^
+ECHO FINE LANDMARK EVALUATION
+
+python run.py --workspace="su_points_333.iws" ^
+              --description="Substudy for number of point controls 333" ^
               --timer=180
 
-python run.py --workspace="td_ffd_ssd.iws" ^
-              --description="Free form deformation with ssd similarity" ^
-              --timer=180
+python run.py --workspace="su_points_444.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=360
 
-python run.py --workspace="td_ffd_mi.iws" ^
-              --description="Free form deformation with ssd similarity" ^
-              --timer=180
+python run.py --workspace="su_points_555.iws" ^
+              --description="Substudy for number of point controls 555" ^
+              --timer=600
+
+python run.py --workspace="su_smooth_0.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=500
+
+python run.py --workspace="su_smooth_01.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=500
+
+python run.py --workspace="su_smooth_0001.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=500
+
+ECHO STEPS
+python run.py --workspace="so_step_1.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=500
+
+python run.py --workspace="so_step_5.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=360
+
+python run.py --workspace="so_step_10.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=500
+
+python run.py --workspace="so_step_20.iws" ^
+              --description="Substudy for number of point controls 444" ^
+              --timer=500
