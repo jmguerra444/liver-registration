6d6823d3d7218f8efedd4c8b2db4c4fbbad0393d
Jorge Guerra <jorge.guerra@surgiceye.com>
2020-11-23 17:16:05 +0100

    Add cropped liver option

--- registration-params/run_robust.py
+++ registration-params/run_robust.py
@@ -6,25 +6,34 @@ import os
 import argparse
 
 from utils import getData, log, screenshot, now, absolutePath
-from helper import setup, kind, filterLandmarks, generateGrid, paramsToString
+from helper import setup, kind, filterLandmarks, generateGrid, paramsToString, parseCrop, moveLandmarks
 
 selectedVolumes = []
 
 # RUN ALL DATASETS ONE WORKSPACE
 run_only = 0
 description = ""
-timer = 300
+timer = 60
 
-workspace = "gn_ffd.iws"
-params = {"similarity" : ["NCC", "MI", "SSD"],
-          "step_size" : [1, 5, 10, 15],
-          "smoothness" : [0, 0.1, 0.01, 0.001, 0.0001]
+# for croping
+lower_limit = parseCrop(0)
+upper_limit = parseCrop(33)
+
+# workspace = "gn_ffd.iws"
+# params = {"similarity" : ["NCC", "MI", "SSD"],
+#           "step_size" : [1, 5, 10, 15],
+#           "smoothness" : [0, 0.1, 0.01, 0.001, 0.0001]
+#          }
+
+workspace = "gn_simple.iws"
+params = { \
+          "similarity" : ["NCC", "MI", "SSD"],
+          "affine" : [1, 0],
+        #   Crop some slices of the volume (in the range are removed)
+          "crop_lower" : [lower_limit],
+          "crop_upper" : [upper_limit]
          }
 
-# workspace = "gn_simple.iws"
-# params = {"similarity" : ["NCC", "MI", "SSD"],
-#           "affine" : [1, 0],
-#          }
 # params = {"similarity" : ["NCC"],
 #           "affine" : [0],
 #          }
@@ -62,6 +71,8 @@ for parameters in grid:
             ctPath = landmarks[p].get("ct-data")
             points1 = landmarks[p].get("points1")
             points2 = landmarks[p].get("points2")
+
+            # points1 = moveLandmarks(points1, upper_limit)   # for the CT
             c = 'ImFusionSuite {} mr="{}" ct="{}" p1="{}" p2="{}" {}'.format(study["workspaceFile"], 
                                                                         mriPath, 
                                                                         ctPath, 
