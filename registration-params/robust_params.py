bf7996dffdd1f3c4f279088a15cb48b6b8865c32
Jorge Guerra <jorge.guerra@surgiceye.com>
2021-03-01 12:37:26 +0100

    Allow running many studies at the same time

--- registration-params/robust_params.py
+++ registration-params/robust_params.py
@@ -1,8 +1,7 @@
 
 from helper import crop
 
-def getParams(key):
-
+def getParams(key, version = 0):
     params = {}
     if (key == "SIMPLE"):
         params = { \
@@ -19,25 +18,49 @@ def getParams(key):
                 }
 
     if (key == "FFD"):
+        params = {}
 
-        ## FFD 1st study
-        # params = {\
-        #         "similarity" : ["MI", "SSD", "NCC", "LNCC"],
-        #         "step_size" : [1, 5, 10, 15],
-        #         "grid_size" : ["3 3 3", "4 4 4", "5 5 5"],
-        #         "smoothness" : [0, 0.1, 0.01, 0.001]
-        #         }
+        if (version == -1):
+            # Test params
+            params = {\
+                    "similarity" : ["NCC"],
+                    "step_size" : [5],
+                    "grid_size" : ["6 6 6"],
+                    "smoothness" : [0]
+                    }
 
-        params = {\
-                "similarity" : ["MI", "SSD", "NCC"],
-                "step_size" : [1, 5, 10, 15],
-                "grid_size" : ["6 6 6"],
-                "smoothness" : [0, 0.01, 0.001]
-                }
+        if (version == 0):
+            # FFD 1st study
+            params = {\
+                    "similarity" : ["MI", "SSD", "NCC", "LNCC"],
+                    "step_size" : [1, 5, 10, 15],
+                    "grid_size" : ["3 3 3", "4 4 4", "5 5 5"],
+                    "smoothness" : [0, 0.1, 0.01, 0.001]
+                    }
+
+        if (version == 1):
+            ## FFD 2nd study
+            params = {\
+                    "similarity" : ["MI", "SSD", "NCC"],
+                    "step_size" : [1, 5, 10, 15],
+                    "grid_size" : ["6 6 6"],
+                    "smoothness" : [0, 0.01, 0.001]
+                    }
+
+        if (version == 2):
+            # FFD 3rd study
+            params = {\
+                    "similarity" : ["NCC", "SSD", "MI"],
+                    "step_size" : [1, 5, 10, 15],
+                    "grid_size" : ["7 7 5"],
+                    "smoothness" : [0, 0.01, 0.001]
+                    }
     return params
 
 def getWorkspace(key):
     workspace = ""
+    if (key == "SEGMENT"):
+        workspace = "0_basic.iws"
     if (key == "SIMPLE"):
         workspace = "gn_simple.iws"
     if (key == "LINEAR"):
--- registration-params/robust_run.py
+++ registration-params/robust_run.py
@@ -11,22 +11,27 @@ from robust_params import getParams, getWorkspace
 
 
 # RUN ALL DATASETS ONE WORKSPACE
-run_only = 0
+run_only = 56
 startFrom = 0
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--key", help = "Key", type = str, default = "FFD")
-parser.add_argument("--timer", help = "Timer", type = int, default = 50)
+parser.add_argument("--timer", help = "Timer", type = int, default = 200)
 args = parser.parse_args()
 timer = args.timer
 key = args.key
 
-params = getParams(key)
+# workspace = getWorkspace("SEGMENT")
 workspace = getWorkspace(key)
 landmarks = getData("landmarks-markus.json")
 
+params = getParams(key, version = -1)
 grid = generateGrid(params)
 grid = grid[startFrom:]
+
+# TO RUN ALL STUDIES
+grid = [generateGrid(getParams(key, version = i)) for i in [0, 1, 2]]
+grid = [*grid[0], *grid[1], *grid[2]]
 
 # ESTIMATE COMPUTATION TIME
 c = 0
@@ -60,24 +65,25 @@ for parameters in grid:
 
         if landmarks[p].get("annotated"):
             log("Doing " + p, study.get("descriptionFile"))
-            mriPath = landmarks[p].get("mri-data") 
+            mriPath = landmarks[p].get("mri-data")
             ctPath = landmarks[p].get("ct-data")
             points1 = landmarks[p].get("points1")
             points2 = landmarks[p].get("points2")
 
             # points1 = moveLandmarks(points1, upper_limit)   # for the CT
-            c = 'ImFusionSuite {} mr="{}" ct="{}" p1="{}" p2="{}" {}'.format(study["workspaceFile"], 
+            c = 'ImFusionSuite {} mr="{}" ct="{}" p1="{}" p2="{}" {}'.format(study["workspaceFile"],
                                                                         mriPath,
                                                                         ctPath,
                                                                         points1,
                                                                         points2,
                                                                         parameters)
-            
+
             pr = subprocess.Popen(c)
             screenshotPath = "{}\\{}.png".format(study["screenshotFolder"], p)
             screenshot(screenshotPath, timer) # Not asynchronous
 
-            if run_only:
-                pr.wait()
-            else:
-                pr.terminate()
+            pr.terminate()
+            # if run_only:
+            #     pr.wait()
+            # else:
+            #     pr.terminate()
